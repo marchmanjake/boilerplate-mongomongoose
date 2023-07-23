5dd271569ae8df61781fb422d1228ad4d8320631
marchmanjake <130931353+marchmanjake@users.noreply.github.com>
2023-07-23 22:46:40 +0000

    Setting up connection with Mongoose db

--- myApp.js
+++ myApp.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
+const mySecret = process.env.MONGO_URI;
+mongoose.connect(mySecret,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 
 let Person;
